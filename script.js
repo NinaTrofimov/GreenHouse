763b987be9740697c95a554cddeb491784f061d9
NinaTrofimov <145512110+NinaTrofimov@users.noreply.github.com>
2024-11-06 17:51:01 -0500

    Updates

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
-$("#document").ready(function(){
+let count = 0;
+
+document.addEventListener('click', function() {
+    const add = document.getElementById('add');
+    const remove = document.getElementById('remove');
     
-})
+    add.onclick = function() {
+        appendItemDetails(); 
+    }
+
+    remove.onclick = function(){
+        removeItemDetails();
+    }
+});
+function appendItemDetails() {
+    count += 1;
+    const div = document.createElement('div');
+    div.id = `itemsDetail${count}`
+    const space = document.getElementById('space');
+    const br = document.createElement('br');
+
+    const itemCodeLabel = document.createElement("label");
+    itemCodeLabel.textContent = "Item Code : ";
+    const itemCodeInput = document.createElement("input");
+    itemCodeInput.id = `code${count}`;
+    itemCodeInput.type = "text";
+    itemCodeInput.required = true;
+
+    const itemNameLabel = document.createElement("label");
+    itemNameLabel.textContent = "Item Name : ";
+    const itemNameInput = document.createElement("input");
+    itemNameInput.id = `name${count}`;
+    itemNameInput.type = "text";
+    itemNameInput.required = true;
+
+    const quantityLabel = document.createElement("label");
+    quantityLabel.textContent = "Quantity : ";
+    const quantityInput = document.createElement("input");
+    quantityInput.id = `quan${count}`;
+    quantityInput.type = "text";
+    quantityInput.required = true;
+
+    const priceLabel = document.createElement("label");
+    priceLabel.textContent = "Total Price : ";
+    const priceInput = document.createElement("input");
+    priceInput.id = `price${count}`;
+    priceInput.type = "text";
+    priceInput.required = true;
+
+    
+    itemCodeLabel.appendChild(itemCodeInput);
+    itemNameLabel.appendChild(itemNameInput);
+    quantityLabel.appendChild(quantityInput);
+    priceLabel.appendChild(priceInput);
+
+    div.appendChild(itemCodeLabel);
+    div.appendChild(itemNameLabel);
+    div.appendChild(quantityLabel);
+    div.appendChild(priceLabel);
+    div.appendChild(br);
+    space.append(div);
+}
+
+function removeItemDetails(div){
+    const previousDiv = document.getElementById(div);
+    if(previousDiv.lastElementChild){
+        previousDiv.remove(previousDiv.lastElementChild);
+    }
+    count -= 1;
+}
