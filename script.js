06d3a749b9ffbfe570a32bcfc45c141762bdd834
NinaTrofimov <145512110+NinaTrofimov@users.noreply.github.com>
2024-12-28 13:12:21 -0500

    fixed script remove

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let count = 0;
 document.addEventListener('click', function() {
     const add = document.getElementById('add');
     const remove = document.getElementById('remove');
+    const submitButton = document.getElementById('subButton');
     
     add.onclick = function() {
         appendItemDetails(); 
@@ -11,43 +12,51 @@ document.addEventListener('click', function() {
     remove.onclick = function(){
         removeItemDetails();
     }
+
+    submitButton.onclick = function(){
+        let infoArray = [];
+        
+    }
+
+
 });
 function appendItemDetails() {
     count += 1;
+
     const div = document.createElement('div');
     div.id = `itemsDetail${count}`
     const space = document.getElementById('space');
     const br = document.createElement('br');
 
     const itemCodeLabel = document.createElement("label");
-    itemCodeLabel.textContent = "Item Code : ";
+    itemCodeLabel.textContent = " Item Code : ";
     const itemCodeInput = document.createElement("input");
     itemCodeInput.id = `code${count}`;
     itemCodeInput.type = "text";
     itemCodeInput.required = true;
 
     const itemNameLabel = document.createElement("label");
-    itemNameLabel.textContent = "Item Name : ";
+    itemNameLabel.textContent = " Item Name : ";
     const itemNameInput = document.createElement("input");
     itemNameInput.id = `name${count}`;
     itemNameInput.type = "text";
     itemNameInput.required = true;
 
     const quantityLabel = document.createElement("label");
-    quantityLabel.textContent = "Quantity : ";
+    quantityLabel.textContent = " Quantity : ";
     const quantityInput = document.createElement("input");
     quantityInput.id = `quan${count}`;
     quantityInput.type = "text";
     quantityInput.required = true;
 
     const priceLabel = document.createElement("label");
-    priceLabel.textContent = "Total Price : ";
+    priceLabel.textContent = " Total Price : ";
     const priceInput = document.createElement("input");
     priceInput.id = `price${count}`;
     priceInput.type = "text";
     priceInput.required = true;
 
-    
+    div.appendChild(br);
     itemCodeLabel.appendChild(itemCodeInput);
     itemNameLabel.appendChild(itemNameInput);
     quantityLabel.appendChild(quantityInput);
@@ -57,12 +66,11 @@ function appendItemDetails() {
     div.appendChild(itemNameLabel);
     div.appendChild(quantityLabel);
     div.appendChild(priceLabel);
-    div.appendChild(br);
     space.append(div);
 }
 
-function removeItemDetails(div){
-    const previousDiv = document.getElementById(div);
+function removeItemDetails(){
+    const previousDiv = document.getElementById(`itemsDetail${count}`);
     if(previousDiv.lastElementChild){
         previousDiv.remove(previousDiv.lastElementChild);
     }
